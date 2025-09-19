d18afbe71dc07c4e746d43d2655ae870f1c3c742
KetanKites <ketan@thekites.ai>
2025-09-19 22:10:22 +0530

    Commit-1: Basic Frontend React Vite + Tailwind Setup Done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-blue-300 justify-center flex items-center h-screen">
+      Hello world!
+    </h1>
+  )
+}
